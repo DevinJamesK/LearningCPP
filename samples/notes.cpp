f2b17ab00db4522c0beccc0c241ac2dd4cbff08e
dkloos <dkloos@globemotors.com>
2017-06-16 12:33:31 -0400

    Added user input function for scale type

--- samples/notes.cpp
+++ samples/notes.cpp
@@ -2,9 +2,10 @@
 
 using namespace std;
 
-// function prototypes
+// function prototypes (these are so functions can be after main)
+string askUserNote();
+int askUserScale();
 int findOffset(string * arr, int size, string target);
-
 
 /*
 * This program takes root note and scale type as user input and returns all
@@ -14,10 +15,6 @@ int main() {
     string notes[12] = 
     {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
     //0,    1    2    3     4    5    6     7    8     9    10    11
-
-    // placeholder for user input (we will add this function later)
-    string userNote = "C";
-    int userScale = 1;
     
     // scale patterns (assume C scale)
     int major[7] = {0, 2, 4, 5, 7, 9, 11};
@@ -26,17 +23,51 @@ int main() {
     // added all scale patters to array that holds arrays of type int
     int * scales[2] = {major, minor};
 
+    // placeholder for user input (we will add this function later)
+    string userNote = askUserNote();
+    int userScale = askUserScale();
+
     //                      arr | size | "C"
     int offset = findOffset(notes, 7, userNote);
+
+    // formatting
+    cout << "----------------------------" << endl;
 
     // prints notes in scale
     for (int i=0; i<7; i++) {
         cout << notes[scales[userScale][i] + offset] << ", ";
     }
 
+    // formatting
     cout << endl;
 
     return 0;
+}
+
+
+/*
+* Asks user for root note, check that is is a valid choice and returns it
+*/
+string askUserNote() {
+    // TODO return dummy data
+    return "C";
+}
+
+/*
+* Prints scale options and user selects one by entering the match number
+*/
+int askUserScale() {
+    int userInput = 0;
+
+    cout << "Select scale type by number: " << endl
+    << "0: Major" << endl
+    << "1: Minor" << endl;
+
+    cin >> userInput;
+
+    // TODO we should check here that the user entered a correct input
+
+    return userInput;
 }
 
 /*
